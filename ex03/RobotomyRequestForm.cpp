4cdc8d972ce86390ad57d1f7e4b8524a0e2f081f
Thomas Parratt <tparratt@c2r2p2.hive.fi>
2024-10-31 13:42:15 +0200

    removed constructor messages

--- ex03/RobotomyRequestForm.cpp
+++ ex03/RobotomyRequestForm.cpp
@@ -3,29 +3,29 @@
 RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", 72, 45)
 {
     this->target = "robotomy_target";
-    std::cout << "RobotomyRequestForm default constructor called" << std::endl;
+    //std::cout << "RobotomyRequestForm default constructor called" << std::endl;
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& obj) : AForm("RobotomyRequestForm", 72, 45)
 {
     this->target = obj.target;
-    std::cout << "RobotomyRequestForm copy constructor called" << std::endl;
+    //std::cout << "RobotomyRequestForm copy constructor called" << std::endl;
 }
 
 RobotomyRequestForm::RobotomyRequestForm(std::string target) : AForm("RobotomyRequestForm", 72, 45)
 {
     this->target = target;
-    std::cout << "RobotomyRequestForm constructor called" << std::endl;
+    //std::cout << "RobotomyRequestForm constructor called" << std::endl;
 }
 
 RobotomyRequestForm::~RobotomyRequestForm()
 {
-    std::cout << "RobotomyRequestForm deconstructor called" << std::endl;
+    //std::cout << "RobotomyRequestForm deconstructor called" << std::endl;
 }
 
 RobotomyRequestForm&  RobotomyRequestForm::operator=(const RobotomyRequestForm& obj)
 {
-    std::cout << "RobotomyRequestForm copy assignment operator called" << std::endl;
+    //std::cout << "RobotomyRequestForm copy assignment operator called" << std::endl;
     if (this != &obj)
     {
         this->target = obj.target;

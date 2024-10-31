0ab0c48adc6c1eba957b98ac7af45ba65c5832b0
Thomas Parratt <tparratt@c2r2p2.hive.fi>
2024-10-31 12:14:55 +0200

    removed constructor comments

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -2,17 +2,17 @@
 
 Form::Form() : name("default_form"), isSigned(false), gradeToSign(0), gradeToExecute(0)
 {
-    std::cout << "Form default constructor called" << std::endl;
+    //std::cout << "Form default constructor called" << std::endl;
 }
 
 Form::Form(const Form& obj) : name(obj.name), isSigned(obj.isSigned), gradeToSign(obj.gradeToSign), gradeToExecute(obj.gradeToExecute)
 {
-    std::cout << "Form copy constructor called" << std::endl;
+    //std::cout << "Form copy constructor called" << std::endl;
 }
 
 Form::Form(std::string name, int gradeToSign, int gradeToExecute) : name(name), isSigned(false), gradeToSign(gradeToSign), gradeToExecute(gradeToExecute)
 {
-    std::cout << "Form constructor called" << std::endl;
+    //std::cout << "Form constructor called" << std::endl;
     if (gradeToSign > 150 || gradeToExecute > 150)
         throw GradeTooLowException();
     else if (gradeToSign < 1 || gradeToExecute < 1)
@@ -21,12 +21,12 @@ Form::Form(std::string name, int gradeToSign, int gradeToExecute) : name(name), 
 
 Form::~Form()
 {
-    std::cout << "Form deconstructor called" << std::endl;
+    //std::cout << "Form deconstructor called" << std::endl;
 }
 
 Form&  Form::operator=(const Form& obj)
 {
-    std::cout << "Form copy assignment operator called" << std::endl;
+    //std::cout << "Form copy assignment operator called" << std::endl;
     if (this != &obj)
     {
         this->isSigned = obj.isSigned;

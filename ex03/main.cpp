51223e36e8ca8876007e83fe5c73487c894ac327
Thomas Parratt <tparratt@c2r2p2.hive.fi>
2024-11-01 14:18:37 +0200

    need to finish main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -7,198 +7,202 @@
 
 int main(void)
 {
-    std::cout << std::endl << "----TRYING TO CREATE NON-EXISTENT FORM----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 146);
-        Intern      someRandomIntern;
-        AForm*      shrub;
+    // std::cout << std::endl << "----TRYING TO CREATE NON-EXISTENT FORM----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 146);
+    //     Intern      someRandomIntern;
+    //     AForm*      shrub;
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        shrub = someRandomIntern.makeForm("WrongForm", "home");
+    //     shrub = someRandomIntern.makeForm("WrongForm", "home");
     
-        std::cout << *shrub << std::endl;
-        shrub->beSigned(b1);
-        std::cout << *shrub << std::endl;
-        shrub->signForm(b1);
-        b1.executeForm(*shrub);
-    }
-    catch(const std::exception& e)
+    //     std::cout << *shrub << std::endl;
+    //     shrub->beSigned(b1);
+    //     std::cout << *shrub << std::endl;
+    //     shrub->signForm(b1);
+    //     b1.executeForm(*shrub);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
     {
-        std::cerr << e.what() << std::endl;
+        AForm*   shrub = nullptr;
+        std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO SIGN SHRUBBERY----" << std::endl << std::endl;
+        try
+        {
+            Bureaucrat  b1("b1", 146);
+            Intern      someRandomIntern;
+
+            std::cout << b1 << std::endl;
+
+            shrub = someRandomIntern.makeForm("ShrubberyCreationForm", "home");
+
+            std::cout << *shrub << std::endl;
+            shrub->beSigned(b1);
+            std::cout << *shrub << std::endl;
+            shrub->signForm(b1);
+            b1.executeForm(*shrub);
+            delete shrub;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+            delete shrub;
+        }
     }
-    std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO SIGN SHRUBBERY----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 146);
-        Intern      someRandomIntern;
-        AForm*      shrub;
+    // std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO EXECUTE SHRUBBERY----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 145);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        shrub = someRandomIntern.makeForm("ShrubberyCreationForm", "home");
+    //     ShrubberyCreationForm   shrub("home");
 
-        std::cout << *shrub << std::endl;
-        shrub->beSigned(b1);
-        std::cout << *shrub << std::endl;
-        shrub->signForm(b1);
-        b1.executeForm(*shrub);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO EXECUTE SHRUBBERY----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 145);
+    //     std::cout << shrub << std::endl;
+    //     shrub.beSigned(b1);
+    //     std::cout << shrub << std::endl;
+    //     shrub.signForm(b1);
+    //     b1.executeForm(shrub);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----EXECUTE SHRUBBERY----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 137);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        ShrubberyCreationForm   shrub("home");
+    //     ShrubberyCreationForm   shrub("home");
 
-        std::cout << shrub << std::endl;
-        shrub.beSigned(b1);
-        std::cout << shrub << std::endl;
-        shrub.signForm(b1);
-        b1.executeForm(shrub);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----EXECUTE SHRUBBERY----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 137);
+    //     std::cout << shrub << std::endl;
+    //     shrub.beSigned(b1);
+    //     std::cout << shrub << std::endl;
+    //     shrub.signForm(b1);
+    //     b1.executeForm(shrub);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO SIGN ROBOTOMY----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 73);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        ShrubberyCreationForm   shrub("home");
+    //     RobotomyRequestForm   robotomy("Bender");
 
-        std::cout << shrub << std::endl;
-        shrub.beSigned(b1);
-        std::cout << shrub << std::endl;
-        shrub.signForm(b1);
-        b1.executeForm(shrub);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO SIGN ROBOTOMY----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 73);
+    //     std::cout << robotomy << std::endl;
+    //     robotomy.beSigned(b1);
+    //     std::cout << robotomy << std::endl;
+    //     robotomy.signForm(b1);
+    //     b1.executeForm(robotomy);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO EXECUTE ROBOTOMY----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 72);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        RobotomyRequestForm   robotomy("Bender");
+    //     RobotomyRequestForm   robotomy("Bender");
 
-        std::cout << robotomy << std::endl;
-        robotomy.beSigned(b1);
-        std::cout << robotomy << std::endl;
-        robotomy.signForm(b1);
-        b1.executeForm(robotomy);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO EXECUTE ROBOTOMY----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 72);
+    //     std::cout << robotomy << std::endl;
+    //     robotomy.beSigned(b1);
+    //     std::cout << robotomy << std::endl;
+    //     robotomy.signForm(b1);
+    //     b1.executeForm(robotomy);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----EXECUTE ROBOTOMY----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 45);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        RobotomyRequestForm   robotomy("Bender");
+    //     RobotomyRequestForm   robotomy("Bender");
 
-        std::cout << robotomy << std::endl;
-        robotomy.beSigned(b1);
-        std::cout << robotomy << std::endl;
-        robotomy.signForm(b1);
-        b1.executeForm(robotomy);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----EXECUTE ROBOTOMY----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 45);
+    //     std::cout << robotomy << std::endl;
+    //     robotomy.beSigned(b1);
+    //     std::cout << robotomy << std::endl;
+    //     robotomy.signForm(b1);
+    //     b1.executeForm(robotomy);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO SIGN PARDON----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 26);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        RobotomyRequestForm   robotomy("Bender");
+    //     PresidentialPardonForm   pardon("Bill");
 
-        std::cout << robotomy << std::endl;
-        robotomy.beSigned(b1);
-        std::cout << robotomy << std::endl;
-        robotomy.signForm(b1);
-        b1.executeForm(robotomy);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO SIGN PARDON----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 26);
+    //     std::cout << pardon << std::endl;
+    //     pardon.beSigned(b1);
+    //     std::cout << pardon << std::endl;
+    //     pardon.signForm(b1);
+    //     b1.executeForm(pardon);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO EXECUTE PARDON----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 25);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        PresidentialPardonForm   pardon("Bill");
+    //     PresidentialPardonForm   pardon("Bill");
 
-        std::cout << pardon << std::endl;
-        pardon.beSigned(b1);
-        std::cout << pardon << std::endl;
-        pardon.signForm(b1);
-        b1.executeForm(pardon);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----BUREAUCRAT GRADE TOO LOW TO EXECUTE PARDON----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 25);
+    //     std::cout << pardon << std::endl;
+    //     pardon.beSigned(b1);
+    //     std::cout << pardon << std::endl;
+    //     pardon.signForm(b1);
+    //     b1.executeForm(pardon);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
+    // std::cout << std::endl << "----EXECUTE PARDON----" << std::endl << std::endl;
+    // try
+    // {
+    //     Bureaucrat  b1("b1", 5);
 
-        std::cout << b1 << std::endl;
+    //     std::cout << b1 << std::endl;
 
-        PresidentialPardonForm   pardon("Bill");
+    //     PresidentialPardonForm   pardon("Bill");
 
-        std::cout << pardon << std::endl;
-        pardon.beSigned(b1);
-        std::cout << pardon << std::endl;
-        pardon.signForm(b1);
-        b1.executeForm(pardon);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << std::endl << "----EXECUTE PARDON----" << std::endl << std::endl;
-    try
-    {
-        Bureaucrat  b1("b1", 5);
-
-        std::cout << b1 << std::endl;
-
-        PresidentialPardonForm   pardon("Bill");
-
-        std::cout << pardon << std::endl;
-        pardon.beSigned(b1);
-        std::cout << pardon << std::endl;
-        pardon.signForm(b1);
-        b1.executeForm(pardon);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    //     std::cout << pardon << std::endl;
+    //     pardon.beSigned(b1);
+    //     std::cout << pardon << std::endl;
+    //     pardon.signForm(b1);
+    //     b1.executeForm(pardon);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    // }
 }
